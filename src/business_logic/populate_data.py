9f2330660b43605a9ec6e2072367d2530a8ca98d
dshmeister <dshmeister@gmail.com>
2024-09-09 00:52:08 +0300

    Changed populate_data.py

--- src/business_logic/populate_data.py
+++ src/business_logic/populate_data.py
@@ -1,21 +1,41 @@
-{
-  "mongo_ip": "localhost",
-  "mongo_port": 27017,
-  "db": "ai_hack",
-  "collections": [
-    {
-      "collection_name": "email_accounts",
-      "docs": [
-        {
-          "name": "account1",
-          "smtp_server": "smtp.ya.ru",
-          "smtp_port": 465,
-          "imap_server": "imap.yandex.ru",
-          "imap_port": 993,
-          "login": "",
-          "password": ""
-        }
-      ]
-    }
-  ]
-}
+import json
+import pymongo
+
+DATA_FILE = "init_data.json"
+
+with open(DATA_FILE, "r") as fd:
+    data = json.load(fd)
+
+mongo_addr = data['mongo_ip']
+mongo_port = data['mongo_port']
+mongo_full_addr = f"mongodb://{mongo_addr}:{mongo_port}/"
+print("Connecting to MongoDB " + mongo_full_addr)
+
+mongo_client = pymongo.MongoClient(mongo_full_addr)
+db_name = data["db"]
+
+if db_name in mongo_client.list_database_names():
+    print("Dropping DB " + db_name)
+    mongo_client.drop_database(db_name)
+
+print("Creating DB " + db_name)
+db = mongo_client[db_name]
+
+for collection in data["collections"]:
+    print(f"Populating {collection['collection_name']} with {len(collection['docs'])} records")
+    mongo_collection = db[collection["collection_name"]]
+    mongo_collection.insert_many(collection["docs"])
+
+print("Creating .env file")
+env_content = [
+    ("MONGO_HOST", mongo_addr),
+    ("MONGO_PORT", mongo_port),
+    ("DB_NAME", db_name)
+]
+
+with open(".env", "w") as fd:
+    for var in env_content:
+        line = var[0] + '="' + str(var[1]) + '"\n'
+        fd.write(line)
+
+print("Done")
