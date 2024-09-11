fd37a9d6a7e883b27504a95c7c2de610c862d448
unknown <Dmitry Zhelobanov>
2024-09-11 19:58:53 +0400

    TG works but we are getting blocked by TG outright

--- src/services/app_logic/tg_utils.py
+++ src/services/app_logic/tg_utils.py
@@ -1,10 +1,56 @@
+TG_TO_SEND_COLLECTION = "send_tg"
+TG_TO_RECV_COLLECTION = "recv_tg"
+
+
 def retrieve_new_tg_message(db):
-    return None
+    collection = db[TG_TO_RECV_COLLECTION]
+    return collection.find_one({"processed": False})
 
 
-def mark_message_processed(db, msg, status):
-    pass
+def mark_message_processed(db, msg, process_status):
+    collection = db[TG_TO_RECV_COLLECTION]
+    collection.update_one(
+        {"_id": msg["_id"]},
+        {"$set": {"processed": True, "process_status": process_status}},
+    )
 
 
 def post_message(db, tg_id, msg, attachment_data=None, attachment_name=None):
-    pass
+    collection = db[TG_TO_SEND_COLLECTION]
+    email = {
+        "tg_id": tg_id,
+        "message": msg,
+        "sent": False,
+    }
+    if attachment_data:
+        email["attachment"] = {"data": attachment_data, "file_name": attachment_name}
+
+    collection.insert_one(email)
+
+
+# def test_tg_server():
+#     import os, pymongo
+#     from bson.binary import Binary
+#
+#     mongo_addr = os.getenv("MONGO_HOST", "localhost")
+#     mongo_port = os.getenv("MONGO_PORT", "27017")
+#     db_name = os.getenv("DB_NAME", "ai_hack")
+#
+#     mongo_full_addr = f"mongodb://{mongo_addr}:{mongo_port}/"
+#     print("Connecting to MongoDB " + mongo_full_addr)
+#     mongo_client = pymongo.MongoClient(mongo_full_addr)
+#     db = mongo_client[db_name]
+#
+#     #post_message(db, "me", "posted message")
+#
+#     # with open("presentation.pdf", "rb") as fd:
+#     #     data = Binary(fd.read())
+#     # post_message(db, "me", "msg with attachment", attachment_data=data, attachment_name="attached_file.pdf")
+#     #
+#     # tg = retrieve_new_tg_message(db)
+#     # print(tg)
+#     # mark_message_processed(db, tg, "ok")
+#
+#
+# if __name__ == "__main__":
+#     test_tg_server()
---
+++ src/services/tg_processor/tg_server.py
@@ -0,0 +1,121 @@
+from telethon import TelegramClient, events
+import motor.motor_asyncio as motor
+import pymongo
+import asyncio, os
+from datetime import datetime, timezone, timedelta
+
+# Use your own values from my.telegram.org
+api_id = 123456
+api_hash = 'hash'
+
+TG_TO_SEND_COLLECTION = "send_tg"
+TG_TO_RECV_COLLECTION = "recv_tg"
+
+# email structure:
+# message
+# optional attachment { data, file_name }
+# tg_id
+# for outgoing tgs
+#   sent
+#   status
+#   process_ts
+# for incoming tgs
+#   processed
+#   recv_ts
+
+
+client = TelegramClient('anon', api_id, api_hash)
+
+
+async def post_message(db, message):
+    recv_collection = db[TG_TO_RECV_COLLECTION]
+    new_tg = {
+        "processed": False,
+        "recv_ts": datetime.now(tz=timezone.utc),
+        "tg_id": message.sender.username,
+        "message": message.text
+    }
+    await recv_collection.insert_one(new_tg)
+
+
+@client.on(events.NewMessage)
+async def new_message_event_handler(event):
+    print("new message:", event.raw_text, ", from ", event.message.sender.username)
+    await post_message(event.client.db, event.message)
+
+
+async def send_tgs(recv_collection):
+    tgs = recv_collection.find({"sent": False})
+    async for tg in tgs:
+        to_update = {"sent": True}
+        try:
+            dst = tg['tg_id']
+            print(f"Sending TG to: {dst}")
+
+            if "attachment" in tg:
+                print("   Sending file", attachment["file_name"])
+                attachment = tg["attachment"]
+                f_path = "/tmp/" + attachment["file_name"]
+                with open(f_path, "wb") as fd:
+                    fd.write(attachment["data"])
+                await client.send_file(dst, file=f_path)
+                os.unlink(f_path)
+
+            await client.send_message(dst, tg["message"])
+            to_update["status"] = "ok"
+        except Exception as e:
+            print("Got exception while sending " + str(e))
+            to_update["status"] = "error: " + str(e)
+
+        to_update["process_ts"] = datetime.now(tz=timezone.utc)
+        recv_collection.update_one({"_id": tg["_id"]},
+                                   {"$set": to_update})
+
+
+async def main():
+    mongo_addr = os.getenv("MONGO_HOST", "localhost")
+    mongo_port = os.getenv("MONGO_PORT", "27017")
+    db_name = os.getenv("DB_NAME", "ai_hack")
+
+    mongo_full_addr = f"mongodb://{mongo_addr}:{mongo_port}/"
+
+    db_client = motor.AsyncIOMotorClient(mongo_full_addr)
+    db = db_client[db_name]
+    client.db = db
+    recv_collection = db[TG_TO_RECV_COLLECTION]
+
+    print("Main started")
+
+    cursor = recv_collection.find().sort("_id", pymongo.DESCENDING).limit(1)
+    last_tg = await cursor.to_list(1)
+    # if len(last_tg) > 0:
+    #     last_ts = last_tg[0]["recv_ts"]
+    # else:
+    last_ts = datetime.now(tz=timezone.utc)
+
+    print("Last recv ts", last_ts)
+    new_messages = []
+    async for dialog in client.iter_dialogs():
+        dialog_messages = []
+        async for message in client.iter_messages(dialog.id):
+            if message.date < last_ts:
+                break
+            # skip ours messages
+            if message.sender_id != dialog.id:
+                continue
+            dialog_messages.append(message)
+        new_messages.extend(dialog_messages[::-1])
+
+    for msg in new_messages:
+        print("Posting old message", msg.text)
+        await post_message(db, msg)
+
+    send_collection = db[TG_TO_SEND_COLLECTION]
+    print("Going into DB polling mode")
+    while True:
+        await send_tgs(send_collection)
+        await asyncio.sleep(1)
+
+
+client.start()
+client.loop.run_until_complete(main())
